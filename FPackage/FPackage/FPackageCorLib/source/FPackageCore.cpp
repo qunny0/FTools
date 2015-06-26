0d474da198a852c49334a614ba312652e2b53c07
cherry <fubei1989@163.com>
2015-06-26 18:30:18 +0800

    commit

---
+++ FPackage/FPackage/FPackageCorLib/source/FPackageCore.cpp
@@ -0,0 +1,12 @@
+#include "FPackageCore.h"
+
+NS_FPACKAGE_BEGIN
+
+IFPackage*	createFPackage(const std::string& filename, uint32_t chunkSize /*= 0x40000*/, uint32_t fileUserDataSize /*= 0*/)
+{
+
+
+	return nullptr;
+}
+
+NS_FPACKAGE_END
--- FPackage/FPackage/FPackageCorLib/source/FPackageCore.h
+++ FPackage/FPackage/FPackageCorLib/source/FPackageCore.h
@@ -1,2 +1,33 @@
 #pragma once
 #include "FPackageDefine.h"
+
+#include <string>
+#include <stdint.h>
+
+NS_FPACKAGE_BEGIN
+
+class IReadFile;
+class IWriteFile;
+
+class IFPackage
+{
+public:
+	
+	virtual std::string& getPackageName() const = 0;
+
+	// readFile
+	virtual bool hasFile(const std::string& filename) const = 0;
+	virtual IReadFile* openFile(const std::string& filename) const = 0;
+
+	// writeFile
+	virtual uint32_t getFileCount() = 0;
+
+};
+
+
+
+//////////////////////////////////////////////////////////////////////////
+
+IFPackage*	createFPackage(const std::string& filename, uint32_t chunkSize = 0x40000, uint32_t fileUserDataSize = 0);
+
+NS_FPACKAGE_END
