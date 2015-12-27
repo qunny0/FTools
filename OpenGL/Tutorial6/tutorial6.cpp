bf23c992e23e4666c5732d3bb6c98c139892a729
qunny <fubei1989@163.com>
2015-12-27 21:10:26 +0800

    Tutorial6 Translation transformation

--- OpenGL/Tutorial6/tutorial6.cpp
+++ OpenGL/Tutorial6/tutorial6.cpp
@@ -1,15 +1,15 @@
-#include <stdio.h>
-#include <assert.h>
-#include <string.h>
-
-#include <math.h>
 #include "glew.h"
 #include "glut.h"
 #include "math_3d.h"
 #include "opengl_util.h"
 
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+#include <math.h>
+
 GLuint VBO;
-GLuint gScaleLocation;
+GLuint gWorldLocation;
 
 const char* pVSFileName = "shader.vs";
 const char* pFSFileName = "shader.fs";
@@ -20,7 +20,15 @@ static void RenderSceneCB()
 
 	static float Scale = 0.0f;
 	Scale += 0.001f;
-	glUniform1f(gScaleLocation, sinf(Scale));
+
+	Matrix4f World;
+
+	World.m[0][0] = 1.0f; World.m[0][1] = 0.0f; World.m[0][2] = 0.0f; World.m[0][3] = sinf(Scale);
+	World.m[1][0] = 0.0f; World.m[1][1] = 1.0f; World.m[1][2] = 0.0f; World.m[1][3] = 0.0f;
+	World.m[2][0] = 0.0f; World.m[2][1] = 0.0f; World.m[2][2] = 1.0f; World.m[2][3] = 0.0f;
+	World.m[3][0] = 0.0f; World.m[3][1] = 0.0f; World.m[3][2] = 0.0f; World.m[3][3] = 1.0f;
+
+	glUniformMatrix4fv(gWorldLocation, 1, GL_TRUE, &World.m[0][0]);
 
 	glEnableVertexAttribArray(0);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
@@ -35,13 +43,14 @@ static void RenderSceneCB()
 
 static void InitializeGlutCallbacks()
 {
- 	glutDisplayFunc(RenderSceneCB);
+	glutDisplayFunc(RenderSceneCB);
 	glutIdleFunc(RenderSceneCB);
 }
 
 static void CreateVertexBuffer()
 {
 	Vector3f Vertices[3] = {};
+
 	Vertices[0] = Vector3f(-1.0f, -1.0f, 0.0f);
 	Vertices[1] = Vector3f(1.0f, -1.0f, 0.0f);
 	Vertices[2] = Vector3f(0.0f, 1.0f, 0.0f);
@@ -54,27 +63,26 @@ static void CreateVertexBuffer()
 static void AddShader(GLuint ShaderProgram, const char* pShaderText, GLenum ShaderType)
 {
 	GLuint ShaderObj = glCreateShader(ShaderType);
-
 	if (ShaderObj == 0)
 	{
 		fprintf(stderr, "Error creating shader type %d\n", ShaderType);
 		exit(1);
 	}
 
-	const GLchar* p[1];
+	const GLchar* p[1] = {};
 	p[0] = pShaderText;
-	GLint Lengths[1];
+	GLint Lengths[1] = {};
 	Lengths[0] = strlen(pShaderText);
+
 	glShaderSource(ShaderObj, 1, p, Lengths);
 	glCompileShader(ShaderObj);
-
-	GLint Success;
-	glGetShaderiv(ShaderObj, GL_COMPILE_STATUS, &Success);
-	if (!Success)
+	GLint success;
+	glGetShaderiv(ShaderObj, GL_COMPILE_STATUS, &success);
+	if (success == 0)
 	{
 		GLchar InfoLog[1024] = {};
 		glGetShaderInfoLog(ShaderObj, 1024, NULL, InfoLog);
-		fprintf(stderr, "Error compiling shader type %d: '%s' \n", ShaderType, InfoLog);
+		fprintf(stderr, "Error compiling shader type: %d '%s' \n", ShaderType, InfoLog);
 		exit(1);
 	}
 
@@ -84,7 +92,6 @@ static void AddShader(GLuint ShaderProgram, const char* pShaderText, GLenum Shad
 static void CompileShaders()
 {
 	GLuint ShaderProgram = glCreateProgram();
-
 	if (ShaderProgram == 0)
 	{
 		fprintf(stderr, "Error creating shader program\n");
@@ -92,7 +99,6 @@ static void CompileShaders()
 	}
 
 	string vs, fs;
-
 	if (!ReadFile(pVSFileName, vs))
 	{
 		exit(1);
@@ -113,23 +119,23 @@ static void CompileShaders()
 	if (Success == 0)
 	{
 		glGetProgramInfoLog(ShaderProgram, sizeof(ErrorLog), NULL, ErrorLog);
-		fprintf(stderr, "Error linking shader program: '%s' \n", ErrorLog);
+		fprintf(stderr, "Error linking shader program: '%s'\n", ErrorLog);
 		exit(1);
 	}
 
 	glValidateProgram(ShaderProgram);
 	glGetProgramiv(ShaderProgram, GL_VALIDATE_STATUS, &Success);
-	if (!Success)
+	if (Success == 0)
 	{
 		glGetProgramInfoLog(ShaderProgram, sizeof(ErrorLog), NULL, ErrorLog);
-		fprintf(stderr, "Invalid shader program: '%s' \n", ErrorLog);
+		fprintf(stderr, "Invalid shader program: '%s'\n", ErrorLog);
 		exit(1);
 	}
 
 	glUseProgram(ShaderProgram);
 
-	gScaleLocation = glGetUniformLocation(ShaderProgram, "gScale");
-	assert(gScaleLocation != 0xFFFFFFFF);
+	gWorldLocation = glGetUniformLocation(ShaderProgram, "gWorld");
+	assert(gWorldLocation != 0xFFFFFFFF);
 }
 
 int main(int argc, char* argv[])
@@ -138,11 +144,10 @@ int main(int argc, char* argv[])
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
 	glutInitWindowSize(1024, 768);
 	glutInitWindowPosition(200, 200);
-	glutCreateWindow("Uniform Variables");
+	glutCreateWindow("Translation Transformation");
 
 	InitializeGlutCallbacks();
 
-	// Must be done after glut is initialized!
 	GLenum res = glewInit();
 	if (res != GLEW_OK)
 	{
@@ -150,17 +155,16 @@ int main(int argc, char* argv[])
 		return 1;
 	}
 
-	printf("GL version: %s\n", glGetString(GL_VERSION));
+	printf("GL Version: %s\n", glGetString(GL_VERSION));
 
 	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 
 	CreateVertexBuffer();
 
- 	CompileShaders();
+	CompileShaders();
 
 	glutMainLoop();
 
 	return 0;
 }
-
 
