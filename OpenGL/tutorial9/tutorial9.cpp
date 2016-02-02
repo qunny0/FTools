21b7bef69aeb89560f298f144d879b8e519a435d
qunny <fubei1989@163.com>
2016-02-02 21:58:08 +0800

    getType clamp(getType x, getType minVal, getType maxVal); min(max(x, minVal), maxVal))
    顶点着色器中返回Color
    
    颜色着色器中接受Color， FragColor = Color

--- OpenGL/tutorial9/tutorial9.cpp
+++ OpenGL/tutorial9/tutorial9.cpp
@@ -6,6 +6,32 @@
 #include <string>
 #include <assert.h>
 
+/************************************************************************/
+/*	
+	Color = vec4(clamp(Position, 0.0, 1.0), 1.0);
+	Color in the graphics pipeline is usually represented using a floating point value in the range[0.0, 1.0]. That value is later 
+	mapped to the integer 0 to 255 for each color channel (totaling in 16M colors). We set the vertex color value as a function of the
+	vertex position. First we using the built-in function clamp() to make sure the values do not go outside of the 0.0-1.0 range. The reason
+	is that the lower left vertex of the triangle is located at -1,-1. If we take that value as is it will be interpolated by the rasterzer 
+	and until both X and Y pass zero we will not see anything because every value which is less than or equal to zero will be rendered as black.
+	This means that half of the edge on each direction will be black before the color pass zero and become something meaningful.
+	By clamping we make only the far bottom left black but as we get further away the color quickly becomes more bright. Try playing with the
+	clamp function remove it all together or change its parameters to see the effect.
+	The result of the clamp function does not go directly to the output variable since it is a 4-vector while the position is a 3-vector
+	(clamp does not change the number of components, only their values). From the point of view of GLSL there is no default conversion here and
+	we have to make this explicit. We do this using the notation 'vec4(vec3, W)' which creates a 4-vector by concatenating a 3-vector with the 
+	supplied W value. In our case we use 1.0 becault this goes into the alpha part of the color and we want the pixel to be completely opaque.
+	
+	getType clamp(getType x, getType minVal, getType maxVal);
+	getTeyp clamp(getTeyp x, float minVal, float maxVal);
+	Returns min(max(x, minVal), maxVal)
+
+	in vec4 Color
+	The opposite side of the output color in the VS is the input color in the FS. 
+	This variable undergoes interpolation by the rasterizer so every FS is (probably) executed with a different color.
+*/
+/************************************************************************/
+
 GLuint VBO;
 GLuint gWorldLocation;
 
