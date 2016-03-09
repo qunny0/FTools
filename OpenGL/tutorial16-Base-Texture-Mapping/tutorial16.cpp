82681630bf9d259d434915b666f72d85e3ca8a25
qunny <fubei1989@163.com>
2016-03-09 22:49:04 +0800

    tutorial16-base-texture-mapping
    ImageMagic -- have not done

---
+++ OpenGL/tutorial16-Base-Texture-Mapping/tutorial16.cpp
@@ -0,0 +1,258 @@
+#include <stdio.h>
+
+#include "glew.h"
+#include "glut.h"
+
+#include "ogldev_pipeline.h"
+#include "ogldev_camera.h"
+#include "ogldev_texture.h"
+#include "ogldev_glut_backend.h"
+
+#define WINDOW_WIDTH	1280
+#define WINDOW_HEIGHT	1024
+
+struct Vertex
+{
+	Vector3f	m_pos;
+	Vector2f	m_tex;
+
+	Vertex() {};
+
+	Vertex(Vector3f pos, Vector2f tex)
+	{
+		m_pos = pos;
+		m_tex = tex;
+	}
+};
+
+GLuint		VBO;
+GLuint		IBO;
+GLuint		gWVPLocation;
+GLuint		gSampler;
+Texture*	pTexture = nullptr;
+Camera*		pGameCamera = nullptr;
+
+PersProjInfo gPersProjInfo;
+
+const char* pVSFileName = "shader.vs";
+const char* pFSFileName = "shader.fs";
+
+static void RenderSceneCB()
+{
+	pGameCamera->OnRender();
+
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	static float scale = 0.0f;
+	scale += 0.1f;
+
+	Pipeline p;
+	p.Rotate(0.0f, scale, 0.0f);
+	p.WorldPos(0.0f, 0.0f, 3.0f);
+	p.SetCamera(*pGameCamera);
+	p.SetPerspectiveProj(gPersProjInfo);
+
+	glUniformMatrix4fv(gWVPLocation, 1, GL_TRUE, (const GLfloat*)p.GetWVPTrans());
+
+	glEnableVertexAttribArray(0);
+	glEnableVertexAttribArray(1);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), 0);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (const GLvoid*)12);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+	pTexture->Bind(GL_TEXTURE0);
+	glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0);
+
+	glDisableVertexAttribArray(0);
+	glDisableVertexAttribArray(1);
+
+	glutSwapBuffers();
+}
+
+static void SpecialKeyboardCB(int key, int x, int y)
+{
+	OGLDEV_KEY ogldevKey = GLUTKeyToOGLDEVKey(key);
+	pGameCamera->OnKeyboard(ogldevKey);
+}
+
+static void KeyboardCB(unsigned char Key, int x, int y)
+{
+// 	switch (Key) {
+// 		case 'q':
+// 			glutLeaveMainLoop();
+// 	}
+}
+
+static void PassiveMouseCB(int x, int y)
+{
+	pGameCamera->OnMouse(x, y);
+}
+
+static void InitializeGlutCallbacks()
+{
+	glutDisplayFunc(RenderSceneCB);
+	glutIdleFunc(RenderSceneCB);
+	glutSpecialFunc(SpecialKeyboardCB);
+	glutPassiveMotionFunc(PassiveMouseCB);
+	glutKeyboardFunc(KeyboardCB);
+}
+
+static void CreateVertexBuffer()
+{
+	Vertex vertices[4] = {
+		Vertex(Vector3f(-1.0f, -1.0f, 0.5774f), Vector2f(0.0f, 0.0f)),
+		Vertex(Vector3f(0.0f, -1.0f, -1.15475f), Vector2f(0.5f, 0.0f)),
+		Vertex(Vector3f(1.0f, -1.0f, 0.5773f), Vector2f(1.0f, 0.0f)),
+		Vertex(Vector3f(0.0f, 1.0f, 0.0f), Vector2f(0.5f, 1.0f)),
+	};
+
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+}
+
+static void CreateIndexBuffer()
+{
+	GLuint indices[] = {
+		0, 3, 1,
+		1, 3, 2,
+		2, 3, 0,
+		0, 1, 2,
+	};
+
+	glGenBuffers(1, &IBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+}
+
+static void addShader(GLuint shaderProgram, const char* pShaderText, GLenum shaderType)
+{
+	GLuint shaderObj = glCreateShader(shaderType);
+
+	if (shaderObj == 0)
+	{
+		fprintf(stderr, "Error creating shader type %d\n", shaderType);
+		exit(1);
+	}
+
+	const GLchar* p[1];
+	p[0] = pShaderText;
+	GLint lengthes[1];
+	lengthes[0] = strlen(pShaderText);
+
+	glShaderSource(shaderObj, 1, p, lengthes);
+	glCompileShader(shaderObj);
+	GLint success;
+	glGetShaderiv(shaderObj, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		GLchar infoLog[1024];
+		glGetShaderInfoLog(shaderObj, 1024, nullptr, infoLog);
+		fprintf(stderr, "Error compiling shader type %d: '%s'\n", shaderType, infoLog);
+		exit(1);
+	}
+
+	glAttachShader(shaderProgram, shaderObj);
+}
+
+static void CompileShaders()
+{
+	GLuint shaderProgram = glCreateProgram();
+
+	if (shaderProgram == 0)
+	{
+		fprintf(stderr, "Error creating shader program\n");
+		exit(1);
+	}
+
+	string fs, vs;
+	if (!ReadFile(pVSFileName, vs))
+	{
+		exit(1);
+	}
+	if (!ReadFile(pFSFileName, fs))
+	{
+		exit(1);
+	}
+
+	addShader(shaderProgram, vs.c_str(), GL_VERTEX_SHADER);
+	addShader(shaderProgram, fs.c_str(), GL_FRAGMENT_SHADER);
+
+	GLint success = 0;
+	GLchar errorLog[1024] = {};
+
+	glLinkProgram(shaderProgram);
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (success == 0)
+	{
+		glGetProgramInfoLog(shaderProgram, sizeof(errorLog), NULL, errorLog);
+		fprintf(stderr, "Error linking shader program: '%s'\n", errorLog);
+		exit(1);
+	}
+
+	glValidateProgram(shaderProgram);
+	glGetProgramiv(shaderProgram, GL_VALIDATE_STATUS, &success);
+	if (success == 0)
+	{
+		glGetProgramInfoLog(shaderProgram, sizeof(errorLog), nullptr, errorLog);
+		fprintf(stderr, "Invalid shader program: '%s'\n", errorLog);
+		exit(1);
+	}
+
+	glUseProgram(shaderProgram);
+
+	gWVPLocation = glGetUniformLocation(shaderProgram, "gWVP");
+	assert(gWVPLocation != 0xFFFFFFFF);
+	gSampler = glGetUniformLocation(shaderProgram, "gSampler");
+	assert(gSampler != 0xFFFFFFFF);
+}
+
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow("Tutorial 17 - Base Texture Mapping");
+
+	InitializeGlutCallbacks();
+
+	pGameCamera = new Camera(WINDOW_WIDTH, WINDOW_HEIGHT);
+
+	// Must be done after glut is initialized!
+	GLenum res = glewInit();
+	if (res != GLEW_OK)
+	{
+		fprintf(stderr, "Error: '%s'\n", glewGetErrorString(res));
+		return 1;
+	}
+
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	glFrontFace(GL_CW);
+	glCullFace(GL_BACK);
+	glEnable(GL_CULL_FACE);
+
+	CreateVertexBuffer();
+	CreateIndexBuffer();
+
+	CompileShaders();
+
+	glUniform1i(gSampler, 0);
+
+	pTexture = new Texture(GL_TEXTURE_2D, "../Content/test.png");
+
+	if (!pTexture->Load())
+	{
+		return 1;
+	}
+
+	gPersProjInfo.FOV = 60.0f;
+	gPersProjInfo.Height = WINDOW_HEIGHT;
+	gPersProjInfo.Width = WINDOW_WIDTH;
+	gPersProjInfo.zNear = 1.0f;
+	gPersProjInfo.zFar = 100.0f;
+
+	glutMainLoop();
+
+	return 0;
+}
