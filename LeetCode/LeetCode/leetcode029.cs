c9fccc30f2a7545fdfa9967cb7287972d30c92dc
FuBei_M <fubei1989@163.com>
2016-11-14 23:44:19 +0800

    临界值的处理问题

--- LeetCode/LeetCode/leetcode029.cs
+++ LeetCode/LeetCode/leetcode029.cs
@@ -2,40 +2,72 @@
 using System.Collections.Generic;
 
 
-// # 1. [1, 1, 2] [1, 2] ; [1, 1]
-// 返回无重复的数组元素长度n，修改nums，前n个数组为不重复的元素
+/**
+ * Error 
+ * # 1 : -1, -1
+ * # 2 : -2147483648 -1
+ * # 3 : -2147483648 -2
+*/
 
 namespace LeetCode
 {
 	public class leetcode029
 	{
 		public class Solution {
+			public Solution(){
+				_halfIntMax = Int32.MaxValue >> 1;
+			}
+
 			public int Divide(int dividend, int divisor) {
 				if (divisor == 0) {
+					return Int32.MaxValue;
+				}
+				if (dividend == Int32.MinValue && divisor == -1) {
 					return Int32.MaxValue;
 				}
 				if (divisor == 1) {
 					return dividend;
 				}
+
+				bool minus = false;
+				if ((dividend < 0 && divisor > 0) || (dividend > 0 && divisor < 0)) {
+					minus = true;
+				}
+				if (dividend < 0) {
+					dividend = Math.Abs (dividend);
+				}
+				if (divisor < 0) {
+					divisor = Math.Abs (divisor);
+				}
+
 				if (dividend < divisor) {
 					return 0;
 				}
 
-//				int tmpHalfValue = Int32.MaxValue >> 1;
-				int tmpDivisor = divisor;
-				int tmpRes = 0;
-				while (true) {
-					tmpDivisor = tmpDivisor << tmpRes;
-					if (dividend >= tmpDivisor) {
-						break;
-					}
-					++tmpRes;
-				}
-				Console.WriteLine ("---34" + tmpRes);
 
-				if (dividend > tmpRes) {
-					
+				int tmpDividend = dividend;
+				int res = 0;
+				while (tmpDividend >= divisor) {
+					int t = twoMinPower (tmpDividend, divisor);
+					int tmpRes = 1;
+					if (t == 0) {
+						if (tmpDividend == divisor) {
+							tmpRes = 1;
+						} else {
+							break;
+						}
+					} else {
+						tmpRes = Convert.ToInt32(Math.Pow (2, t-1));
+					}
+
+					tmpDividend = tmpDividend - divisor * tmpRes;
+					res += tmpRes;
 				}
+
+				if (minus) {
+					return -res;
+				}
+				return res;
 			}
 
 			/**
@@ -51,17 +83,23 @@ namespace LeetCode
 			 * # 8: (2147483647, 3)
 			*/
 			public int twoMinPower(int big, int small) {
+				if (small == 0) {
+					return 0;
+				}
+
 				int k = 0;
 				while (true) {
-					small = small << k;
-					if (big < small) {
+					if (big <= small || small >= _halfIntMax) {
 						return k;
 					}
+					small = small << 1;
 					++k;
 				}
 
 				return k;
 			}
+
+			static private int _halfIntMax;
 		}
 
 		class MainClass
@@ -69,6 +107,24 @@ namespace LeetCode
 			public static void Main (string[] args)
 			{
 				Solution s = new Solution();
+
+//				int t1 = s.twoMinPower (0, 1);
+//				int t2 = s.twoMinPower (1, 0);
+//				int t3 = s.twoMinPower (0, 0);
+//				int t4 = s.twoMinPower (1, 1);
+//				int t5 = s.twoMinPower (10, 3);
+//				int t6 = s.twoMinPower (12, 2);
+//				int t7 = s.twoMinPower (2147482645, 2);
+//				int t8 = s.twoMinPower (2147483647, 3);
+
+//				int t1 = s.Divide (0, 1);
+//				int t2 = s.Divide (1, 0);
+//				int t3 = s.Divide (0, 0);
+//				int t4 = s.Divide (1, 1);
+				int t5 = s.Divide (-2147483648, -1);
+				int t6 = s.Divide (12, 2);
+				int t7 = s.Divide (2147482645, 2);
+				int t8 = s.Divide (2147483647, 3);
 
 				int idx = s.Divide (1111, 2);
 				Console.WriteLine ("" + idx);
