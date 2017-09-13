a153352326034103770fc0649ed8bd58a667af89
ben from rayjoy iMac <ben@bens-iMac.local>
2017-09-13 12:08:03 +0800

    Tetris 添加一些UI的元素

---
+++ UnityTutorial/Tetris/Tetris/Assets/_Script/Game.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Game : MonoBehaviour {
+
+	public Text txtScore;
+
+	public GameObject nextCellShow;
+
+	// Use this for initialization
+	void Start () {
+		txtScore.text = "0";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void setNextCell (GameObject cell) {
+		
+	}
+
+	void setScore (int score) {
+		txtScore.text =  "" + score;
+	}
+}
