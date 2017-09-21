6fd195b44a697a141f18b2eedd0ca7460fca9cde
ben from rayjoy iMac <ben@bens-iMac.local>
2017-09-21 11:42:19 +0800

    代码都有了，进行调试。

--- UnityTutorial/Tetris/Roguelike/Assets/Scripts/GameManager.cs
+++ UnityTutorial/Tetris/Roguelike/Assets/Scripts/GameManager.cs
@@ -1,8 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
+
+	public float levelStartDelay = 2f;
+	public float turnDelay = 0.1f;
+	public int playerFoodPoints = 100;
 
 	public static GameManager instance = null;
 
@@ -10,9 +15,17 @@ public class GameManager : MonoBehaviour {
 
 	private int level = 3;
 
-	public int playerFoodPoints = 100;
-
 	[HideInInspector] public bool playersTurn = true;
+
+	private List<Enemy> enemies;
+
+	private bool enemiesMoving;
+
+
+	private Text levelText;
+	private GameObject levelImage;
+	private bool doingSetup = true;
+
 
 	// Awake is always called before any Start functions
 	void Awake () {
@@ -26,16 +39,81 @@ public class GameManager : MonoBehaviour {
 		//
 		DontDestroyOnLoad(gameObject);
 
+		enemies = new List<Enemy> ();
+
 		boardScript = GetComponent<BoardManager> ();
 
 		InitGame ();
 	}
 
+	void OnLevelWasLoaded(int index) {
+		level++;
+
+		InitGame ();
+	}
+
 	public void GameOver (int level) {
-		
+		levelText.text = "After " + level + " days, you starved.";
+
+		levelImage.SetActive (true);
+
+		enabled = false;
 	}
 
 	void InitGame() {
+
+		doingSetup = true;
+
+		levelImage = GameObject.Find ("LevelImage");
+
+		levelText = GameObject.Find ("LevelText").GetComponent<Text> ();
+
+		levelText.text = "Day " + level;
+
+		levelImage.SetActive (true);
+
+		Invoke ("HideLevelImage", levelStartDelay);
+
+
+		enemies.Clear ();
+		
 		boardScript.SetupScene (level);
+	}
+
+	void HideLevelImage() {
+		levelImage.SetActive (false);
+
+		doingSetup = false;
+	}
+
+	public void AddEnemeyToList(Enemy script) {
+		enemies.Add (script);
+	}
+
+	void Update() {
+		if (playersTurn || enemiesMoving)
+			return;
+
+		StartCoroutine (MoveEnemies ());
+	}
+
+	IEnumerator MoveEnemies() {
+		enemiesMoving = true;
+
+		yield return new WaitForSeconds (turnDelay);
+
+		if (enemies.Count == 0) {
+			yield return new WaitForSeconds (turnDelay);
+		}
+
+		for (int i = 0; i < enemies.Count; i++) {
+			enemies[i].MoveEnemy();
+				
+			yield return new WaitForSeconds(enemies[i].moveTime);
+		}
+
+		playersTurn = true;
+
+		enemiesMoving = false;
 	}
 }
--- UnityTutorial/Tetris/Roguelike/Assets/Scripts/Player.cs
+++ UnityTutorial/Tetris/Roguelike/Assets/Scripts/Player.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Player : MovingObject {
 
@@ -13,11 +14,24 @@ public class Player : MovingObject {
 	private Animator animator;
 	private int food;
 
+	public Text foodText;
+
+	public AudioClip moveSound1;
+	public AudioClip moveSound2;
+	public AudioClip eatSound1;
+	public AudioClip eatSound2;
+	public AudioClip drinkSound1;
+	public AudioClip drinkSound2;
+	public AudioClip gameOverSound;
+
+	private Vector2 touchOrigin = -Vector2.one;
 
 	protected override void Start() {
 		animator = GetComponent<Animator> ();
 
 		food = GameManager.instance.playerFoodPoints;
+
+		foodText.text = "Food: " + food;
 
 		base.Start ();
 	}
@@ -28,12 +42,14 @@ public class Player : MovingObject {
 
 	// Update is called once per frame
 	void Update () {
-//		if (!GameManager.instance.playersTurn)
-//			return;
+		if (!GameManager.instance.playersTurn)
+			return;
 
 		// Used to store the horizontal move direction
 		int horizontal = 0;
 		int vertical = 0;
+
+		#if UNITY_STANDALONE || UNITY_WEBPLAYER
 
 		horizontal = (int)(Input.GetAxisRaw ("Horizontal"));
 		vertical = (int)(Input.GetAxisRaw ("Vertical"));
@@ -41,6 +57,31 @@ public class Player : MovingObject {
 		if (horizontal != 0) {
 			vertical = 0;
 		}
+
+		#elif UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE
+
+		if (Input.touchCount > 0) {
+			Touch myTouch = Input.touches[0];
+			
+			if (myTouch.TouchPhase == TouchPhase.Began) {
+				touchOrigin = myTouch.Position;
+			} else if (myTouch.phase == TouchPhase.Ended && touchOrigin.x >= 0) {
+				Vector2 touchEnd = myTouch.Position;
+
+				float x = touchEnd.x - touchOrigin.x;
+
+				float y = touchEnd.y - touchOrigin.y;
+
+				touchOrigin.x = -1;
+
+				if (Math.Abs(x) > Math.Abs(y)) {
+					horizontal = x > 0 ? 1 : -1;
+				} else {
+					vertical = y > 0 ? 1 : -1;
+				}
+			}
+		}
+		#endif
 
 		if (horizontal != 0 || vertical != 0) {
 			// Call AttemptMove passing in the generic parameter Wall, since that is what Player may interact with if they
@@ -50,9 +91,11 @@ public class Player : MovingObject {
 		}
 	}
 
-	protected override void AttemptMove <T> (int xDir, int yDir) {
+	protected override void AttemptMove<T> (int xDir, int yDir) {
 		// Every time player moves, subtract from food points total.
 		food--;
+
+		foodText.text = "Food: " + food;
 
 		base.AttemptMove<T> (xDir, yDir);
 
@@ -84,9 +127,13 @@ public class Player : MovingObject {
 		} else if (other.tag == "Food") {
 			food += pointsPerFood;
 
+			foodText.text = " + " + pointsPerFood + " Food: " + food;
+
 			other.gameObject.SetActive (false);
 		} else if (other.tag == "Soda") {
 			food += pointsPerSoda;
+
+			foodText.text = " + " + pointsPerSoda + " Food: " + food;
 
 			other.gameObject.SetActive (false);
 		}
@@ -100,6 +147,8 @@ public class Player : MovingObject {
 		animator.SetTrigger ("playerHit");
 
 		food -= loss;
+
+		foodText.text = " - " + loss + " Foods: " + food;
 
 		CheckIfGameOver ();
 	}
